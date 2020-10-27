4d5d4ebab36a3170fa5d1cc17bf35f96adb05b22
Reza Amini <rezaaminiroyal@gmail.com>
2020-10-27 15:27:01 +0330

    extract facades to a base

---
+++ Support/Contract/BaseFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AdminPanel\Support\Contract;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class BaseFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return static::class;
+    }
+
+    public static function shouldProxyTo($class)
+    {
+        app()->singleton(static::getFacadeAccessor(), $class);
+    }
+}
