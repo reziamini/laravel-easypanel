ab4e8c008d13c0dfec45b0cd6b28193bc98cffeb
Reza Amini <rezaaminiroyal@gmail.com>
2020-10-28 22:35:23 +0330

    update configs

--- config/admin_panel_config.php
+++ config/admin_panel_config.php
@@ -2,14 +2,28 @@
 
 return [
 
+    // Enable the todo feature
+    'todo' => true,
+    'todo_model' => \App\Models\Todo::class,
+
+    // How to authenticate admin
+    // You may use other ways to authenticate a admin (tables or ..) you can manage it with this class
     'auth_class' => \AdminPanel\Support\Auth\ColumnAuth::class,
+
+    // With this class you can manage how to create a admin or remove it.
     'admin_provider_class' => \AdminPanel\Support\User\UserProvider::class,
+
+    // if You use a column in your users table to identify admins , put column name here
     'column' => 'is_superuser',
 
+    // it's a place where a user if not authenticated will be redirected
     'redirect_unauthorized' => '/',
 
+    // Admin panel routes prefix
     'route_prefix' => 'admin',
 
+
+    // CRUD manager
     'actions' => [
         'user' => [
             'model' => \App\Models\User::class,
