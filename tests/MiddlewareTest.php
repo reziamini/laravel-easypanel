fc38e1d1c356ef01d02765cbd8d996a8d5125011
Reza Amini <rezaaminiroyal@gmail.com>
2020-11-06 12:28:55 +0330

    add middleware tests

---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace AdminPanelTest;
+
+
+use AdminPanel\Http\Middleware\isAdmin;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MiddlewareTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test * */
+    public function user_is_unauthorized(){
+        \Illuminate\Support\Facades\Route::get('/admin')->middleware(isAdmin::class);
+        $this->actingAs($this->user)->get('/admin');
+
+        $middleware = new isAdmin();
+        $res = $middleware->handle(request(), function (){});
+
+        $this->assertEquals($res->getStatusCode(), 302);
+    }
+
+    /** @test * */
+    public function user_is_valid(){
+        \Illuminate\Support\Facades\Route::get('/admin')->middleware(isAdmin::class);
+        $this->user->update([
+            'is_superuser' => true
+        ]);
+
+        $this->actingAs($this->user->refresh())->get('/admin');
+        $middleware = new isAdmin();
+        $res = $middleware->handle(request(), function (){});
+
+        $this->assertEquals($res, null);
+    }
+}
