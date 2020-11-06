d0d9fcdfbbdb1543bdaab2c7543756201e604c34
Reza Amini <rezaaminiroyal@gmail.com>
2020-11-06 12:04:35 +0330

    update testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,15 @@
 namespace AdminPanelTest;
 
 use AdminPanel\AdminPanelServiceProvider;
+use AdminPanelTest\Dependencies\User;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+
+    /**
+     * @var \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    protected $user;
 
     protected function setUp(): void
     {
@@ -13,6 +19,8 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
         $this->loadMigrationsFrom(__DIR__.'/Dependencies/database/migrations');
         $this->withFactories(__DIR__.'/Dependencies/database/factories');
+        $this->user = factory(User::class)->create(['is_superuser' => false]);
+        config()->set('admin_panel.user_model', User::class);
     }
 
     protected function getPackageProviders($app)
