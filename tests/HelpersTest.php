07c694d4dfcdf835463cc21a480e5c5c37c75c88
Reza Amini <rezaaminiroyal@gmail.com>
2020-11-07 12:02:25 +0330

    add test for helpers

---
+++ tests/HelpersTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace EasyPanelTest;
+
+
+class HelpersTest extends TestCase
+{
+
+    /** @test * */
+    public function get_icon_helper_works(){
+        $this->assertEquals(get_icon('user'), 'user');
+        $this->assertEquals(get_icon('users'), 'user');
+        $this->assertEquals(get_icon('admin'), 'user');
+        $this->assertEquals(get_icon('article'), 'file-text');
+        $this->assertEquals(get_icon('articles'), 'file-text');
+        $this->assertEquals(get_icon('posts'), 'file-text');
+        $this->assertEquals(get_icon('xxxx'), 'grid');
+    }
+
+    /** @test * */
+    public function get_route_name_works(){
+        config()->set('easy_panel.route_prefix', 'admin');
+        $this->assertEquals(getRouteName(), 'admin');
+        config()->set('easy_panel.route_prefix', 'admin/panel');
+        $this->assertEquals(getRouteName(), 'admin.panel');
+        config()->set('easy_panel.route_prefix', 'admin/panel/dashboard');
+        $this->assertEquals(getRouteName(), 'admin.panel.dashboard');
+    }
+
+}
