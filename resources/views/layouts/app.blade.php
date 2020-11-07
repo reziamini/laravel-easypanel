0a01bd50f8b44e9216d34a27858c4564b7bb3a62
Reza Amini <rezaaminiroyal@gmail.com>
2020-11-07 20:21:24 +0330

    add cogout feature in dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,14 +104,10 @@
                                                                               class="svg-icon"></i></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
-                            <a class="dropdown-item" href="javascript:void(0)"><i data-feather="user"
-                                                                                  class="svg-icon mr-2 ml-1"></i>
-                                My Profile</a>
-
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="javascript:void(0)"><i data-feather="power"
+                            <a class="dropdown-item" href="javascript:void(0)" onclick="event.preventDefault(); document.querySelector('#logout').submit()"><i data-feather="power"
                                                                                   class="svg-icon mr-2 ml-1"></i>
                                 Logout</a>
+                            <form id="logout" action="@route(getRouteName().'.logout')" method="post"> @csrf </form>
                         </div>
                     </li>
                     <!-- ============================================================== -->
