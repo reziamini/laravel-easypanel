4165eb6c88fe8710b4b30eaca4c74d63ab3d0bd6
Reza Amini <rezaaminiroyal@gmail.com>
2020-11-10 19:55:41 +0330

    add tests for create todo

--- tests/TodoFeatureTest.php
+++ tests/TodoFeatureTest.php
@@ -3,6 +3,7 @@
 namespace EasyPanelTest;
 
 use EasyPanel\Http\Livewire\Todo\Create;
+use EasyPanel\Models\Todo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
@@ -17,8 +18,24 @@ class TodoFeatureTest extends TestCase
 
         Livewire::test(Create::class)
             ->set('title', 'Contributing in')
-            ->call('create');
-
+            ->call('create')
+            ->assertDispatchedBrowserEvent('show-message');
         $this->assertDatabaseHas('todos', ['title' => 'Contributing in']);
+
+        Livewire::test(Create::class)
+            ->set('title', 'Contributing in')
+            ->call('create');
+        $this->assertCount(1, Todo::all());
     }
+
+    /** @test * */
+    public function create_with_empty_title(){
+        $this->actingAs($this->user);
+
+        Livewire::test(Create::class)
+            ->set('title', '')
+            ->call('create')
+            ->assertHasErrors('title');
+    }
+
 }
