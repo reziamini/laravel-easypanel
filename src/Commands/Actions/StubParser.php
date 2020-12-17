956a40868c5e6486cb37bb26f349aa72e9ef5193
Reza Amini <rezaaminiroyal@gmail.com>
2020-12-17 21:46:53 +0330

    add upload file to update mode

--- src/Commands/Actions/StubParser.php
+++ src/Commands/Actions/StubParser.php
@@ -89,7 +89,7 @@ trait StubParser
         $str = '';
         foreach ($filesInput as $file) {
             $storePath = $this->getConfig('store')[$file] ?? "{$file}/";
-            $str .= $this->makeTab(2).'if($this->getPropertyValue(\''.$file.'\')) {'.$this->makeTab(3);
+            $str .= $this->makeTab(2).'if($this->getPropertyValue(\''.$file.'\') and is_object($this->'.$file.')) {'.$this->makeTab(3);
             $str .= '$this->'.$file.' = $this->getPropertyValue(\''.$file.'\')->store(\''.$storePath.'\');'.$this->makeTab(2);
             $str .= '}'.PHP_EOL;
         }
