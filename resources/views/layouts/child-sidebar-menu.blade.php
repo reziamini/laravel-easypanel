3469668aa356e9e0cd87be19f3c179710688c1df
Reza Amini <rezaaminiroyal@gmail.com>
2021-02-09 22:15:51 +0330

    update menu style

--- resources/views/layouts/child-sidebar-menu.blade.php
+++ resources/views/layouts/child-sidebar-menu.blade.php
@@ -2,10 +2,22 @@
 <li class="nav-small-cap"><span class="hide-menu">{{ __('CRUD Menu') }}</span></li>
 @foreach(config('easy_panel.actions') as $name)
     <li class='sidebar-item @isActive([getRouteName().".$name.read", getRouteName().".$name.create", getRouteName().".$name.update"], "selected")'>
-        <a class='sidebar-link @isActive([getRouteName().".$name.read", getRouteName().".$name.create", getRouteName().".$name.update"], "active") ' href="@route(getRouteName().'.'.$name.'.read')" aria-expanded="false">
+        <a class='sidebar-link has-arrow' href="javascript:void(0)" aria-expanded="false">
             <i data-feather="{{ get_icon($name) }}" class="feather-icon"></i>
             <span class="hide-menu">{{ __(\Illuminate\Support\Str::ucfirst($name)) }}</span>
         </a>
+        <ul aria-expanded="false" class="collapse first-level base-level-line">
+            <li class="sidebar-item @isActive(getRouteName().'.'.$name.'.read')">
+                <a href="@route(getRouteName().'.'.$name.'.read')" class="sidebar-link @isActive(getRouteName().'.'.$name.'.read')">
+                    <span class="hide-menu"> {{ __('List') }} </span>
+                </a>
+            </li>
+            <li class="sidebar-item @isActive(getRouteName().'.'.$name.'.create')">
+                <a href="@route(getRouteName().'.'.$name.'.create')" class="sidebar-link @isActive(getRouteName().'.'.$name.'.create')">
+                    <span class="hide-menu"> {{ __('Create') }} </span>
+                </a>
+            </li>
+        </ul>
     </li>
 @endforeach
 
