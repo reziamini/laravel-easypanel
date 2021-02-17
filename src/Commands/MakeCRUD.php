9a57b42d886fa1661eeaf5c868fbf3d113e93b82
Reza Amini <rezaaminiroyal@gmail.com>
2021-02-17 12:25:00 +0330

    refactor crud creator

--- src/Commands/MakeCRUD.php
+++ src/Commands/MakeCRUD.php
@@ -15,31 +15,23 @@ class MakeCRUD extends Command
     public function handle()
     {
         $names = $this->argument('name') ? [$this->argument('name')] : config('easy_panel.actions', []);
-        if($names == null) {
+
+        if(is_null($names)) {
             throw new CommandNotFoundException("There is no action in config file");
         }
+
         foreach ($names as $name) {
-            $config = config('easy_panel.crud.' . $name);
+            $args = ['name' => $name, '--force' => $this->option('force')];
+
+            $config = config("easy_panel.crud.$name");
+
             if (!$config) {
                 throw new CommandNotFoundException("There is no {$name} in config file");
             }
 
             $this->modelNameIsCorrect($name, $config['model']);
 
-            if (!$config['create']) {
-                $this->warn("The create action is disabled for {$name}");
-            } else {
-                $this->call('panel:create', ['name' => $name, '--force' => $this->option('force')]);
-            }
-
-            if (!$config['update']) {
-                $this->warn("The update action is disabled for {$name}");
-            } else {
-                $this->call('panel:update', ['name' => $name, '--force' => $this->option('force')]);
-            }
-
-            $this->call('panel:read', ['name' => $name, '--force' => $this->option('force')]);
-            $this->call('panel:single', ['name' => $name, '--force' => $this->option('force')]);
+            $this->createActions($config, $name, $args);
         }
     }
 
@@ -51,6 +43,25 @@ class MakeCRUD extends Command
         if($model != $name){
             throw new CommandNotFoundException("Action key should be equal to model name, You are using {$name} as key name but your model name is {$model}");
         }
+    }
+
+
+    private function createActions($config, $name, $args)
+    {
+        if (isset($config['create']) and $config['create']) {
+            $this->call('panel:create', $args);
+        } else {
+            $this->warn("The create action is disabled for {$name}");
+        }
+
+        if (isset($config['update']) and $config['update']) {
+            $this->call('panel:update', $args);
+        } else {
+            $this->warn("The update action is disabled for {$name}");
+        }
+
+        $this->call('panel:read', $args);
+        $this->call('panel:single', $args);
     }
 
 }
