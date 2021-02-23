08a8e353bb819d4198f7acfe3f406a38553621d2
Reza Amini <rezaaminiroyal@gmail.com>
2021-02-23 17:00:48 +0330

    fix select problem

--- src/Parsers/HTMLInputs/Select.php
+++ src/Parsers/HTMLInputs/Select.php
@@ -7,20 +7,10 @@ class Select
 
     public function handle($name, $values, $action)
     {
-
-        if(!class_exists($values[0])) {
-            return "<select wire:model='$name' class=\"form-control @error('$name') is-invalid @enderror\" id='input$name'>
-                    @foreach(config('easy_panel.crud.$action.fields.$name')['select'] as " . '$key => $value' . ")
-                        <option value='{{ " . '$key' . " }}'>{{ " . '$value' . " }}</option>
-                    @endforeach
-                </select>";
-        }
-
         return "<select wire:model='$name' class=\"form-control @error('$name') is-invalid @enderror\" id='input$name'>
-                    @foreach((new $values[0])->pluck('$values[2]', '$values[1]') as " . '$key => $value' . ")
-                        <option value='{{ " . '$key' . " }}'>{{ " . '$value' . " }}</option>
-                    @endforeach
-                </select>";
-
+                @foreach(config('easy_panel.crud.$action.fields.$name')['select'] as " . '$key => $value' . ")
+                    <option value='{{ " . '$key' . " }}'>{{ " . '$value' . " }}</option>
+                @endforeach
+            </select>";
     }
 }
