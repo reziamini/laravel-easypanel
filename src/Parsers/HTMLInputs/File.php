13e0b266c9af5172a7b91ff9a8165546e084eaff
Reza Amini <rezaaminiroyal@gmail.com>
2021-03-19 11:54:17 +0330

    add image preview in view

--- src/Parsers/HTMLInputs/File.php
+++ src/Parsers/HTMLInputs/File.php
@@ -2,11 +2,17 @@
 
 namespace EasyPanel\Parsers\HTMLInputs;
 
+use Illuminate\Support\Str;
+
 class File
 {
 
     public function handle($name)
     {
-        return "<input type='file' wire:model='$name' class=\"form-control-file @error('$name') is-invalid @enderror\" id='input$name'>";
+        $string = '$'.$name;
+        return "<input type='file' wire:model='$name' class=\"form-control-file @error('$name') is-invalid @enderror\" id='input$name'>
+                @if($string and !".'$errors'."->has('$name') and $string instanceof \Livewire\TemporaryUploadedFile and (in_array( ".$string."->guessExtension(), ['png', 'jpg', 'gif', 'jpeg'])))
+                    <a href=\"{{ ".$string."->temporaryUrl() }}\"><img width=\"200\" height=\"200\" class=\"img-fluid shadow\" src=\"{{ ".$string."->temporaryUrl() }}\" alt=\"\"></a>
+                @endif";
     }
 }
