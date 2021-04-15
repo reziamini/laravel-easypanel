05f5fd39b6831ab03b91c1973c418704d17752f3
Reza Amini <rezaaminiroyal@gmail.com>
2021-04-15 18:36:19 +0430

    Fix meddleware problem

--- src/Http/Middleware/isAdmin.php
+++ src/Http/Middleware/isAdmin.php
@@ -10,6 +10,10 @@ class isAdmin
 
     public function handle($request, Closure $next)
     {
+        if(auth()->guest()){
+            return redirect(config('easy_panel.redirect_unauthorized'));
+        }
+
         if(!AuthFacade::checkIsAdmin(auth()->user()->id)){
             return redirect(config('easy_panel.redirect_unauthorized'));
         }
