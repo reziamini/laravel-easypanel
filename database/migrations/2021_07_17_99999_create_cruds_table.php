28d52c3b83d1a91b920fe479161b26eebecb31ff
Reza Amini <rezaaminiroyal@gmail.com>
2021-07-17 23:15:52 +0430

    Prepare CRUD manager dependency

---
+++ database/migrations/2021_07_17_99999_create_cruds_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrudsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cruds', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('model')->unique();
+            $table->string('route')->unique();
+            $table->boolean('active')->default(true);
+            $table->boolean('built')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cruds');
+    }
+}
