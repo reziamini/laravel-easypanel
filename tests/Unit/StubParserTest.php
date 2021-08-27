7ec5ff71dd509bc18c548059bb077a90119d5bf0
Reza Amini <rezaaminiroyal@gmail.com>
2021-08-27 13:41:24 +0430

    Add some unit tests for stub parser

--- tests/Unit/StubParserTest.php
+++ tests/Unit/StubParserTest.php
@@ -97,4 +97,57 @@ class StubParserTest extends \EasyPanelTest\TestCase
         $this->assertStringContainsString($expected, $this->parser->parseTitlesInBlade());
     }
 
+    /** @test * */
+    public function relational_field_names_will_be_parsed_successfully(){
+        $field = 'user.name';
+        $expected = 'User Name';
+        $this->assertEquals($expected, $this->parser->parseFieldNameWithDots($field));
+
+        $field = 'user.image.file';
+        $expected = 'User Image File';
+        $this->assertEquals($expected, $this->parser->parseFieldNameWithDots($field));
+    }
+
+    /** @test * */
+    public function dots_will_be_replaced_with_arrow(){
+        $field = 'user.name';
+        $expected = 'user->name';
+        $this->assertEquals($expected, $this->parser->parseDots($field));
+    }
+
+    /** @test * */
+    public function properties_will_be_filled_for_actions(){
+        $this->parser->setInputs([
+            'title' => 'text',
+            'body' => 'ckeditor',
+        ]);
+
+        $expected1 = '$this->title = $this->article->title;'."\n";
+        $expected2 = '$this->body = $this->article->body;';
+        $parsedString = $this->parser->parseSetPropertiesValue();
+        $this->assertStringContainsString($expected1, $parsedString);
+        $this->assertStringContainsString($expected2, $parsedString);
+    }
+
+    /** @test * */
+    public function texts_will_be_set_to_translate(){
+        $this->parser->setInputs([
+            'title' => 'text',
+            'body' => 'ckeditor',
+            'image' => 'file',
+        ]);
+        $this->parser->parseInputsInBlade();
+        $this->assertContains('Title', $this->parser->texts);
+        $this->assertContains('Body', $this->parser->texts);
+        $this->assertContains('Image', $this->parser->texts);
+
+        $this->parser->setFields(['title', 'image', 'user.name']);
+        $this->parser->parseTitlesInBlade();
+        $this->assertContains('User Name', $this->parser->texts);
+        $this->assertContains('Title', $this->parser->texts);
+        $this->assertContains('Image', $this->parser->texts);
+
+        $this->assertCount(4, $this->parser->texts);
+    }
+
 }
