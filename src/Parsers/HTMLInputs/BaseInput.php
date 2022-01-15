b6f832d5ab84274ad50f86c0ba8fad4e6ccbca7f
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-15 23:10:44 +0330

    Change InputParser strategy

--- src/Parsers/HTMLInputs/BaseInput.php
+++ src/Parsers/HTMLInputs/BaseInput.php
@@ -4,27 +4,79 @@ namespace EasyPanel\Parsers\HTMLInputs;
 
 abstract class BaseInput
 {
-    protected $name;
+    protected $key;
     protected $action;
+    protected $mode;
+    protected $label;
+    protected $placeholder;
 
-    public function __construct($name, $action)
+    public function __construct($label)
     {
-        $this->name = $name;
+        $this->label = $label;
+        $this->mode = config('easy_panel.lazy_mode') ? 'wire:model.lazy' : 'wire:model';
+    }
+
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function setAction($action)
+    {
         $this->action = $action;
+
+        return $this;
+    }
+
+    public function placeholder($placeholder)
+    {
+        $this->placeholder = $placeholder;
+
+        return $this;
+    }
+
+    public static function label($label)
+    {
+        return new static($label);
+    }
+
+    public function lazyMode()
+    {
+        $this->mode = 'wire:model.lazy';
+
+        return $this;
+    }
+
+    public function deferMode()
+    {
+        $this->mode = 'wire:model.defer';
+
+        return $this;
     }
 
     public function render()
     {
-        $mode = config('easy_panel.lazy_mode') ? 'wire:model.lazy' : 'wire:model';
-
         $array = [
-            '{{ Title }}' => ucfirst($this->name),
-            '{{ Name }}' => $this->name,
-            '{{ Mode }}' => $mode,
+            '{{ Title }}' => $this->label,
+            '{{ Name }}' => $this->key,
+            '{{ Mode }}' => $this->mode,
             '{{ Action }}' => $this->action,
+            '{{ placeholder }}' => $this->placeholder,
         ];
 
         return str_replace(array_keys($array), array_values($array), file_get_contents(__DIR__.'/stubs/'.$this->stub));
+    }
+
+    public function getTitle()
+    {
+        return $this->label;
+    }
+
+    public function getPlaceholder()
+    {
+        return $this->placeholder;
     }
 
 }
