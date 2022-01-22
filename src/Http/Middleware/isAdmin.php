bfef5c8bceced3e4d484025b83be08318d17894f
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-22 14:20:08 +0330

    Set default value for config in middleware

--- src/Http/Middleware/isAdmin.php
+++ src/Http/Middleware/isAdmin.php
@@ -10,14 +10,14 @@ class isAdmin
 
     public function handle($request, Closure $next)
     {
-        auth()->shouldUse(config('easy_panel.auth_guard'));
+        auth()->shouldUse(config('easy_panel.auth_guard', 'web'));
 
         if(auth()->guest()){
-            return redirect(config('easy_panel.redirect_unauthorized'));
+            return redirect(config('easy_panel.redirect_unauthorized', '/'));
         }
 
         if(!AuthFacade::check(auth()->user()->id)){
-            return redirect(config('easy_panel.redirect_unauthorized'));
+            return redirect(config('easy_panel.redirect_unauthorized', '/'));
         }
 
         return $next($request);
