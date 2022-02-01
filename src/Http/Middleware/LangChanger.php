09db72f19ce7cf74f2dcb5315655cb00097a64bd
Reza Amini <rezaaminiroyal@gmail.com>
2022-02-01 14:16:58 +0330

    Change lang changer middleware strategy

--- src/Http/Middleware/LangChanger.php
+++ src/Http/Middleware/LangChanger.php
@@ -4,16 +4,18 @@ namespace EasyPanel\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 class LangChanger
 {
 
     public function handle($request, Closure $next)
     {
-        $lang = config('easy_panel.lang') ?? 'en';
-        $langFormat = $lang.'_panel';
+        $lang = session()->has('easypanel_lang')
+            ? session()->get('easypanel_lang')
+            : (config('easy_panel.lang') ?? 'en') .'_panel';
 
-        App::setLocale($langFormat);
+        App::setLocale($lang);
 
         return $next($request);
     }
