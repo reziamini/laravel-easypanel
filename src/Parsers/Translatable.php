fbfafa27873d943bc03db3f6042e076b8dcc84cc
Reza Amini <rezaaminiroyal@gmail.com>
2022-02-08 18:38:57 +0330

    Add translatable trait

---
+++ src/Parsers/Translatable.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace EasyPanel\Parsers;
+
+
+use EasyPanel\Services\LangManager;
+
+trait Translatable
+{
+    protected $texts = [];
+
+    public function addText($text, $key = null)
+    {
+        if (array_key_exists($key, $this->texts)){
+            return;
+        }
+
+        $this->texts[$key ?? $text] = $text;
+    }
+
+    public function translate()
+    {
+        LangManager::update($this->texts);
+    }
+
+    public function getTexts()
+    {
+        return $this->texts;
+    }
+}
