e8effed8f4fad916546269ac7e0d7487ff3a98a2
Reza Amini <rezaaminiroyal@gmail.com>
2022-02-28 21:45:07 +0330

    Add tests for panel:admins command

--- tests/Feature/ManageAdminWithCommandTest.php
+++ tests/Feature/ManageAdminWithCommandTest.php
@@ -5,6 +5,7 @@ namespace EasyPanelTest\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use EasyPanelTest\TestCase;
+use EasyPanel\Support\Contract\UserProviderFacade;
 
 class ManageAdminWithCommandTest extends TestCase
 {
@@ -31,6 +32,27 @@ class ManageAdminWithCommandTest extends TestCase
         ]);
 
         $this->assertFalse( (bool) $this->user->panelAdmin()->exists());
+    }
+
+    /** @test * */
+    public function all_admins_is_listed(){
+        Artisan::call('panel:add', [
+            'user' => $this->user->id
+        ]);
+
+        $this->artisan('panel:admins')
+            ->expectsOutput("• {$this->user->name}: {$this->user->email}");
+    }
+
+    /** @test * */
+    public function all_super_users_are_returned(){
+        Artisan::call('panel:add', [
+            'user' => $this->user->id,
+            '--super' => true
+        ]);
+
+        $this->artisan('panel:admins')
+            ->expectsOutput("• {$this->user->name}: {$this->user->email} ( Super Admin ✅ )");
     }
 
 }
