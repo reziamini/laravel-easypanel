3e7c3a7ef0805f5623ef987b2b1b2b30d408d2b2
Reza Amini <rezaaminiroyal@gmail.com>
2022-02-28 22:16:45 +0330

    Add more tests for user provider commands

--- tests/Feature/ManageAdminWithCommandTest.php
+++ tests/Feature/ManageAdminWithCommandTest.php
@@ -21,6 +21,35 @@ class ManageAdminWithCommandTest extends TestCase
     }
 
     /** @test * */
+    public function message_will_be_written_from_user_provider(){
+        UserProviderFacade::shouldReceive('makeAdmin')
+            ->with($this->user->id, true)
+            ->once()
+            ->andReturn([
+                'type' => 'success',
+                'message' => "Done",
+            ]);
+
+        $this->artisan('panel:add', [
+            'user' => $this->user->id,
+            '--super' => true
+        ])->expectsOutput('Done');
+    }
+
+    /** @test * */
+    public function it_shows_error_when_an_exception_is_thrown(){
+        UserProviderFacade::shouldReceive('makeAdmin')
+            ->with($this->user->id, true)
+            ->once()
+            ->andThrow(\Exception::class, 'Error');
+
+        $this->artisan('panel:add', [
+            'user' => $this->user->id,
+            '--super' => true
+        ])->expectsOutput("Something went wrong!\nError: Error");
+    }
+
+    /** @test * */
     public function remove_admin_with_command(){
         Artisan::call('panel:add', [
             'user' => $this->user->id
@@ -32,6 +61,13 @@ class ManageAdminWithCommandTest extends TestCase
         ]);
 
         $this->assertFalse( (bool) $this->user->panelAdmin()->exists());
+    }
+
+    /** @test * */
+    public function it_asks_to_remove_an_admin(){
+        $this->artisan('panel:remove', [
+            'user' => 1
+        ])->expectsConfirmation("Do you want to remove 1 from administration");
     }
 
     /** @test * */
