d233924e5fc99094e5af542cc550aa39c0e89feb
Reza Amini <rezaaminiroyal@gmail.com>
2022-08-05 02:07:12 +0430

    Fix uploader bug

--- src/Parsers/StubParser.php
+++ src/Parsers/StubParser.php
@@ -7,6 +7,7 @@ use EasyPanel\Parsers\HTMLInputs\InputList;
 use EasyPanel\Parsers\Fields\Field;
 use EasyPanel\Parsers\HTMLInputs\BaseInput;
 use EasyPanel\Concerns\Translatable;
+use EasyPanel\Parsers\HTMLInputs\File;
 
 class StubParser
 {
@@ -114,16 +115,19 @@ class StubParser
      */
     public function uploadCodeParser()
     {
-        $filesInput = array_keys($this->inputs, 'file');
         $str = '';
-        foreach ($filesInput as $file) {
+        foreach ($this->inputs as $key => $input) {
+            $inputObject = $this->normalizeInput($key, $input);
+            if (! $inputObject instanceof File){
+                continue;
+            }
             // We get store path which has been defined in crud's config file
-            $storePath = $this->store[$file] ?? "{$file}";
+            $storePath = $this->store[$key] ?? "{$key}";
 
             // We get property value then store file in $storePath
             // A PHP Code for upload as a string will be created here
-            $str .= $this->makeTab(2).'if($this->getPropertyValue(\''.$file.'\') and is_object($this->'.$file.')) {'.$this->makeTab(3);
-            $str .= '$this->'.$file.' = $this->getPropertyValue(\''.$file.'\')->store(\''.$storePath.'\');'.$this->makeTab(2);
+            $str .= $this->makeTab(2).'if($this->getPropertyValue(\''.$key.'\') and is_object($this->'.$key.')) {'.$this->makeTab(3);
+            $str .= '$this->'.$key.' = $this->getPropertyValue(\''.$key.'\')->store(\''.$storePath.'\');'.$this->makeTab(2);
             $str .= '}'.PHP_EOL;
         }
 
